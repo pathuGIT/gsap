bd3602cb086cbc6c0c6c09d1f3579dbd13c57ff0
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2023-08-28 23:23:11 +0530

    scroll trigger

--- 06_scroll_I/App.js
+++ 06_scroll_I/App.js
@@ -1,7 +1,31 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.to(".c",{
-    scrollTrigger: ".c",
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".b",
+        toggleActions: "restart pause reverse none",
+        start: "top center",
+        markers: true,
+        scrub:true,
+        // pin:".ghost"
+        pin: true
+    }
+})
+
+tl.to(".b",{
+    x:400,
     rotation:360,
     duration:3,
-    dellay:2
+    ease: "none",
+    backgroundColor: "purple",
 })
+
+
+// .to(".b",{
+//     duration: 1,
+// })
+
+// .to(".b",{
+//     x:0,
+//     duration: 3,
+// })
